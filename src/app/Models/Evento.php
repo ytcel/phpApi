30c2512a74c4b15cc96704ac928f21106f8a40a9
ytcel <ytcel.dominguez@gmail.com>
2026-01-13 03:23:23 +0000

    CREATED estructura base

---
+++ src/app/Models/Evento.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Evento extends Model
+{
+    protected $table='eventos';
+    protected $fillable=['titulo','descripcion','fecha_inicio','fecha_fin','ubicacion'];
+}
